b11fb8db02462af5a025961f25111a9125636edd
fluffykoo <oumou.camara@efrei.net>
2025-11-21 16:17:47 +0100

    migration  est visitée pour le modèle Salles

--- AuthenticationServices/Migrations/20251121151047_AddEstVisiteeToSalle.Designer.cs
+++ AuthenticationServices/Migrations/20251121151047_AddEstVisiteeToSalle.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AuthenticationServices.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AuthenticationServices.Migrations
 {
     [DbContext(typeof(AventureDbContext))]
-    partial class AventureDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121151047_AddEstVisiteeToSalle")]
+    partial class AddEstVisiteeToSalle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AuthenticationServices/Migrations/20251121151047_AddEstVisiteeToSalle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstVisiteeToSalle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Salles_Donjons_DonjonId",
+                table: "Salles");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DonjonId",
+                table: "Salles",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EstVisitee",
+                table: "Salles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ForceMonstre",
+                table: "Salles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageMonstre",
+                table: "Salles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomMonstre",
+                table: "Salles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PvMonstre",
+                table: "Salles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Salles_Donjons_DonjonId",
+                table: "Salles",
+                column: "DonjonId",
+                principalTable: "Donjons",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Salles_Donjons_DonjonId",
+                table: "Salles");
+
+            migrationBuilder.DropColumn(
+                name: "EstVisitee",
+                table: "Salles");
+
+            migrationBuilder.DropColumn(
+                name: "ForceMonstre",
+                table: "Salles");
+
+            migrationBuilder.DropColumn(
+                name: "ImageMonstre",
+                table: "Salles");
+
+            migrationBuilder.DropColumn(
+                name: "NomMonstre",
+                table: "Salles");
+
+            migrationBuilder.DropColumn(
+                name: "PvMonstre",
+                table: "Salles");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DonjonId",
+                table: "Salles",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Salles_Donjons_DonjonId",
+                table: "Salles",
+                column: "DonjonId",
+                principalTable: "Donjons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
