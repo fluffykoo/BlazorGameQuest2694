d741f94980fe6e16360f14b0896c50b4764c4f57
Lucas BAURY <lucas.baury@efrei.net>
2025-12-08 21:24:12 +0100

    ajout docker-compose et gateway

---
+++ BlazorGame.Gateway/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Ajout de YARP
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+app.MapReverseProxy();
+
+app.Run();
